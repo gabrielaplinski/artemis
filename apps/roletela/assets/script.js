262584d58268bd3f792cc59e446949cc0ffaf506
Gabriela Piovesan Plinski <gabrielaplinski@outlook.com>
2026-02-12 00:12:48 -0300

    Update script.js

--- apps/roletela/assets/script.js
+++ apps/roletela/assets/script.js
@@ -7,8 +7,8 @@ const listaTitulos = [
     {titulo: 'Fratura', plataforma: 'Netflix'},
     {titulo: 'Made in Abyss', plataforma: 'Netflix'},
     {titulo: 'Hellsing', plataforma: ['Netflix', 'Prime Video']},
-    {titulo: 'Fratura', plataforma: 'Netflix'},
-    {titulo: 'Fratura', plataforma: 'Netflix'},
-    {titulo: 'Fratura', plataforma: 'Netflix'},
-    {titulo: 'Fratura', plataforma: 'Netflix'},
+    {titulo: 'Um contratempo', plataforma: 'Prime Video'},
+    {titulo: 'Kanata no Astra', plataforma: 'Crunchyroll'},
+    {titulo: 'Kusuriya no Hitorigoto', plataforma: ['Netflix', 'Crunchyroll', 'Prime Video', 'HBO Max']},
+    {titulo: 'Bleach', plataforma: ['Disney', 'Crunchyroll', 'Prime Video']},
 ]
