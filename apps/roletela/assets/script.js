fc30a367bc526d4047bbaa80b0c471a5d6c4b145
Gabriela Piovesan Plinski <gabrielaplinski@outlook.com>
2026-02-11 23:57:16 -0300

    Update script.js

---
+++ apps/roletela/assets/script.js
@@ -0,0 +1,14 @@
+// ver se o filme/serie já tá no sorteio com sugestões  
+// filtro por plataforma de stream
+
+// filme fratura  plataforma netflix
+
+const listaTitulos = [
+    {titulo: 'Fratura', plataforma: 'Netflix'},
+    {titulo: 'Fratura', plataforma: 'Netflix'},
+    {titulo: 'Fratura', plataforma: 'Netflix'},
+    {titulo: 'Fratura', plataforma: 'Netflix'},
+    {titulo: 'Fratura', plataforma: 'Netflix'},
+    {titulo: 'Fratura', plataforma: 'Netflix'},
+    {titulo: 'Fratura', plataforma: 'Netflix'},
+]
