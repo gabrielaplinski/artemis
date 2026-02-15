e1f7317c78699cfe5668adc57e4bd28ef1dddd84
ralflnk <rafaellunkes_@hotmail.com>
2026-02-14 23:41:27 -0300

    flask

--- roletela/backend/functions.py
+++ roletela/backend/functions.py
@@ -1,56 +1,31 @@
 import json
    
-def LerLista():
+def listarTitulos():
     try:
-        arquivo = open("./roletela/assets/filmes.json", "r")
+        arquivo = open("./roletela/backend/filmes.json", "r")
     except:
-        arquivo = open("./roletela/assets/filmes.json", "w")
+        arquivo = open("./roletela/backend/filmes.json", "w")
     try:
         dados = json.load(arquivo)
     except:
-        print('arquivo vazio')
         dados = []
     arquivo.close()
-    return dados
-    
-def AdicionarAoJson(nome, canal):
-    dados = LerLista()
+    return dados  
+
+def adicionarTitulo(titulo, plataforma):
+    dados = listarTitulos()
     dados.append({
         "id": len(dados) + 1,
-        "titulo": nome,
-        "plataforma": canal
+        "titulo": titulo,
+        "plataforma": plataforma
     })
-    arquivo = open("./roletela/assets/filmes.json", "w")
+    arquivo = open("./roletela/backend/filmes.json", "w")
     json.dump(dados, arquivo)
     arquivo.close()
-    
-def CadastrarFilme():
-    while True:
-        nome = input("Digite o nome do filme: ").capitalize()
-        canal = input("Digite a plataforma do filme: ").capitalize()
-        AdicionarAoJson(nome, canal)
-        continuar = input("Deseja cadastrar outro filme? (s/n): ")
-        if continuar.lower() == "s":
-            continue
-        elif continuar.lower() == "n":
-            break
-        else:
-            print("Resposta inválida. Por favor, digite 's' para sim ou 'n' para não.")
+    return "Filme cadastrado com sucesso!"
 
-def ListarFilmes():
-    dados = LerLista()
-    for filme in dados:
-        print(f'{filme["id"]} - {filme["titulo"]} : {filme["plataforma"]}')
-
-def Sorteio():
+def sortearTitulo():
     import random
-    dados = LerLista()
-    filme_sorteado = random.choice(dados)
-    separator = '-' * 30
-    print(separator)
-    print(f'Filme sorteado: {filme_sorteado["titulo"]} - Plataforma: {filme_sorteado["plataforma"]}')
-    print(separator)
-    
-CadastrarFilme()
-ListarFilmes()
-Sorteio()
+    dados = listarTitulos()
+    filme_sorteado = random.choice(dados)    
+    return filme_sorteado
--- roletela/backend/roletela.py
+++ roletela/backend/roletela.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template, request
+from functions import *
 
 app = Flask(__name__, template_folder='../')
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route('/sortear', methods=['GET'])
+def sorteio():
+    return jsonify(sortearTitulo())
+
+@app.route('/filmes', methods=['GET'])
+def filmes():
+    return jsonify(listarTitulos())
+
+@app.route('/filmes', methods=['POST'])
+def cadastrar_filme():
+    dados = request.get_json()
+    titulo = dados.get('titulo')
+    plataforma = dados.get('plataforma')
+    return jsonify(adicionarTitulo(titulo, plataforma))
 
 if __name__ == '__main__':
     app.run(debug=True)
