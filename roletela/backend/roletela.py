80d6a6bb475281b44f737eff7cbc318f58ad96c1
ralflnk <rafaellunkes_@hotmail.com>
2026-03-24 00:19:05 -0300

    python

--- roletela/backend/roletela.py
+++ roletela/backend/roletela.py
@@ -65,11 +65,13 @@ def alterar_status_flask():
 def add_assistindo_flask():
     dados = request.get_json()
     id_api = dados.get('id_api')
-    return jsonify(add_assistindo(id_api))
+    add_assistindo(id_api)
+    return '',200
 
 @app.route('/assistindo', methods=['DELETE'])
 def remover_assistindo_flask():
-    return remover_assistindo()
+    remover_assistindo()
+    return '',200
 
 @app.route('/assistindo', methods=['GET'])
 def assistindo_flask():
